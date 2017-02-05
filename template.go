5a5aaea8f6f926db8f0d2f3ffa81b161bc9ed69c
Yuqian Yang <yyang@freewheel.tv>
2017-02-05 14:42:15 +0800

    TestTemplate_render

--- template.go
+++ template.go
@@ -1,8 +1,10 @@
 package genus
 
 import (
+	"bytes"
 	"errors"
 	"io/ioutil"
+	"text/template"
 )
 
 type Template struct {
@@ -13,6 +15,7 @@ type Template struct {
 	SkipExists  bool   // skip generation if exist
 	SkipFormat  bool   // skip go format
 	rawTemplate []byte // rawTemplate data in bytes
+	rawResult   []byte
 }
 
 // Set raw template data
@@ -21,7 +24,7 @@ func (tmpl *Template) SetRawTemplate(raw []byte) (data []byte) {
 	return
 }
 
-// load raw template data from file
+// Load raw template data from file
 func (tmpl *Template) loadFile() (data []byte, err error) {
 	if tmpl.Source == "" {
 		return nil, errors.New("Empty source path")
@@ -33,5 +36,22 @@ func (tmpl *Template) loadFile() (data []byte, err error) {
 	}
 
 	tmpl.rawTemplate = data
+	return
+}
+
+// Render template by context
+func (tmpl *Template) render(context interface{}) (data []byte, err error) {
+	parsed, parsedErr := template.New(tmpl.Name).Parse(string(tmpl.rawTemplate))
+	if parsedErr != nil {
+		return nil, parsedErr
+	}
+
+	buf := bytes.NewBuffer([]byte{})
+	if execErr := parsed.Execute(buf, context); execErr != nil {
+		return nil, execErr
+	}
+	data = buf.Bytes()
+
+	tmpl.rawResult = data
 	return
 }
--- template_test.go
+++ template_test.go
@@ -27,9 +27,14 @@ func TestTemplate_loadFile(t *testing.T) {
 			TargetDir: "_test",
 			Filename:  "t1.go",
 		}, []byte("package main\nfunc T1(){}\n"), false},
-		{"OK", fields{
+		{"KO", fields{
 			Name:      "t1",
 			Source:    "./testdata/template/not-exist-t1.tpl",
+			TargetDir: "_test",
+			Filename:  "t1.go",
+		}, nil, true},
+		{"KO", fields{
+			Name:      "t1",
 			TargetDir: "_test",
 			Filename:  "t1.go",
 		}, nil, true},
@@ -51,6 +56,62 @@ func TestTemplate_loadFile(t *testing.T) {
 		}
 		if !reflect.DeepEqual(gotData, tt.wantData) {
 			t.Errorf("%q. Template.loadFile() = %v, want %v", tt.name, string(gotData), string(tt.wantData))
+		}
+	}
+}
+
+func TestTemplate_render(t *testing.T) {
+	type fields struct {
+		Name        string
+		Source      string
+		TargetDir   string
+		Filename    string
+		SkipExists  bool
+		SkipFormat  bool
+		rawTemplate []byte
+	}
+	type args struct {
+		context interface{}
+	}
+	tests := []struct {
+		name     string
+		fields   fields
+		args     args
+		wantData []byte
+		wantErr  bool
+	}{
+		{"OK", fields{
+			Name:        "t1",
+			Source:      "./testdata/template/t1.tpl",
+			TargetDir:   "_test",
+			Filename:    "t1.go",
+			rawTemplate: []byte("package {{ .Package }}"),
+		}, args{map[string]string{"Package": "main"}}, []byte("package main"), false},
+		{"KO", fields{
+			Name:        "t1",
+			Source:      "./testdata/template/t1.tpl",
+			TargetDir:   "_test",
+			Filename:    "t1.go",
+			rawTemplate: []byte("package {{ .Package"),
+		}, args{map[string]string{"Package": "main"}}, nil, true},
+	}
+	for _, tt := range tests {
+		tmpl := &Template{
+			Name:        tt.fields.Name,
+			Source:      tt.fields.Source,
+			TargetDir:   tt.fields.TargetDir,
+			Filename:    tt.fields.Filename,
+			SkipExists:  tt.fields.SkipExists,
+			SkipFormat:  tt.fields.SkipFormat,
+			rawTemplate: tt.fields.rawTemplate,
+		}
+		gotData, err := tmpl.render(tt.args.context)
+		if (err != nil) != tt.wantErr {
+			t.Errorf("%q. Template.render() error = %v, wantErr %v", tt.name, err, tt.wantErr)
+			continue
+		}
+		if !reflect.DeepEqual(gotData, tt.wantData) {
+			t.Errorf("%q. Template.render() = %v, want %v", tt.name, string(gotData), string(tt.wantData))
 		}
 	}
 }
