cb5e1cdb34fb48a6346dd9fb39945012afd2f6f3
Yuqian Yang <yyang@freewheel.tv>
2017-02-05 19:01:57 +0800

    TestTemplateGroup_ensureGopath

--- template_group.go
+++ template_group.go
@@ -1,30 +1,52 @@
 package genus
 
-// "log"
+import (
+	"errors"
+	"log"
+	"os"
+	"path/filepath"
+	"runtime"
+	"strings"
+)
 
 type Imports map[string]string
 
 // Represents single Go package
 type TemplateGroup struct {
 	Package        string
+	BaseDir        string // absolute directory for template group
+	BasePackage    string
 	Imports        Imports
 	Templates      []*Template
 	SkipFixImports bool
 }
 
-// // TODO: V1 doesn't support windows
-// func (tg *TemplateGroup) ensureGOOS() (err error) {
-// 	if runtime.GOOS == "windows" {
-// 		return errors.New("Windows is not supported for now")
-// 	}
-//
-// 	return
-// }
-//
-// // Operations must be performed under gopath
-// func (tg *TemplateGroup) ensureGopath() (err error) {
-// 	gopath := os.Getenv("GOPATH")
-// 	strings.Split(gopath, ":")
-//
-// 	return
-// }
+// TODO: V1 doesn't support windows
+func (tg *TemplateGroup) ensureGOOS() (err error) {
+	if runtime.GOOS == "windows" {
+		return errors.New("Windows is not supported for now")
+	}
+
+	return
+}
+
+// Operations must be performed under gopath
+func (tg *TemplateGroup) ensureGopath() (err error) {
+	gopaths := strings.Split(os.Getenv("GOPATH"), ":")
+	pwd, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+
+	for _, gopath := range gopaths {
+		if strings.HasPrefix(pwd, gopath) {
+			log.Printf("Ensure %s under $GOPATH", pwd)
+			tg.BaseDir = pwd
+			tg.BasePackage = strings.TrimRight(pwd, filepath.Join(gopath, "src"))
+
+			return nil
+		}
+	}
+
+	return errors.New("Run outside gopath")
+}
---
+++ template_group_test.go
@@ -0,0 +1,63 @@
+package genus
+
+import "testing"
+
+func TestTemplateGroup_ensureGOOS(t *testing.T) {
+	type fields struct {
+		Package        string
+		BaseDir        string
+		Imports        Imports
+		Templates      []*Template
+		SkipFixImports bool
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		wantErr bool
+	}{
+		{"OK", fields{}, false},
+	}
+	for _, tt := range tests {
+		tg := &TemplateGroup{
+			Package:        tt.fields.Package,
+			BaseDir:        tt.fields.BaseDir,
+			Imports:        tt.fields.Imports,
+			Templates:      tt.fields.Templates,
+			SkipFixImports: tt.fields.SkipFixImports,
+		}
+		if err := tg.ensureGOOS(); (err != nil) != tt.wantErr {
+			t.Errorf("%q. TemplateGroup.ensureGOOS() error = %v, wantErr %v", tt.name, err, tt.wantErr)
+		}
+	}
+}
+
+func TestTemplateGroup_ensureGopath(t *testing.T) {
+	type fields struct {
+		Package        string
+		BaseDir        string
+		BasePackage    string
+		Imports        Imports
+		Templates      []*Template
+		SkipFixImports bool
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		wantErr bool
+	}{
+		{"OK", fields{}, false},
+	}
+	for _, tt := range tests {
+		tg := &TemplateGroup{
+			Package:        tt.fields.Package,
+			BaseDir:        tt.fields.BaseDir,
+			BasePackage:    tt.fields.BasePackage,
+			Imports:        tt.fields.Imports,
+			Templates:      tt.fields.Templates,
+			SkipFixImports: tt.fields.SkipFixImports,
+		}
+		if err := tg.ensureGopath(); (err != nil) != tt.wantErr {
+			t.Errorf("%q. TemplateGroup.ensureGopath() error = %v, wantErr %v", tt.name, err, tt.wantErr)
+		}
+	}
+}
